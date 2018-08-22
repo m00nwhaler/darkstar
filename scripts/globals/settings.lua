98bd1f64b55935554b61ab8bad6fd0d7fdc96d1b
TeoTwawki <arawn.annwn@yandex.com>
2018-08-22 12:56:57 -0400

    Remove DIMENSIONAL_PORTAL_UNLOCK This is going to conflict with commit 2effc1f62dabf9a2d5d0ce6d54ea594e7990ad79 in the other branch doing the same thing, which I thought would be merged by now..
    > Erase DIMENSIONAL_PORTAL_UNLOCK setting from existence - just comment out the mission check if you want this feature.
    >
    > Present me questions how past me got things merged sometimes.

--- scripts/globals/settings.lua
+++ scripts/globals/settings.lua
@@ -148,4 +148,3 @@ FORCE_SPAWN_QM_RESET_TIME = 300; -- Number of seconds the ??? remains hidden for
 
 -- LIMBUS
 BETWEEN_2COSMOCLEANSE_WAIT_TIME = 20; -- Hours before player can re-enter Limbus. Default is 20 hrs.
-DIMENSIONAL_PORTAL_UNLOCK = false; -- Set true to bypass requirements for using dimensional portals to reach sea for Limbus
